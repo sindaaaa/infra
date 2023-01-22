9e0652f2a76234bce0d8126945fd76bcc2d61367
DhiaGh <96071963+DhiaGh@users.noreply.github.com>
2023-01-22 21:00:57 +0100

    Update S3_push.py

--- S3_push.py
+++ S3_push.py
@@ -3,8 +3,8 @@ import os
 
 client = boto3.client(
     's3',
-    aws_access_key_id="AKIAYSYPVKMUBZDFF7WP",
-    aws_secret_access_key="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"
+    aws_access_key_id="AWS_ACCES_KEY",
+    aws_secret_access_key="AWS_SECRET_ACCES_KEY"
 )
 
 bucket_name = 'cbdprojectjoinbucket'
