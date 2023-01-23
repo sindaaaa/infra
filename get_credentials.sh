17132582d221991dbfd0b78387d6a4074b5a79a1
DhiaGh <96071963+DhiaGh@users.noreply.github.com>
2023-01-23 03:30:26 +0100

    Add files via upload

---
+++ get_credentials.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# usage: ./get_csv_element.sh <filename> <column> <row>
+
+#unset env VAR:
+unset AWS_ACCESS_KEY
+unset AWS_SECRET_ACCESS_KEY
+
+# Assign the input arguments to variables
+filename=$1
+
+# Use awk to extract the specific element
+element_key=$(awk -F, -v col="1" -v row="2" '{if (NR==row) print $col}' "$filename")
+
+element_secret=$(awk -F, -v col="2" -v row="2" '{if (NR==row) print $col}' "$filename")
+
+
+# Print the element
+export AWS_ACCESS_KEY=$element_key >> ~/.bashrc
+export AWS_SECRET_ACCESS_KEY=$element_secret >> ~/.bashrc
+
+source ~/.bashrc
