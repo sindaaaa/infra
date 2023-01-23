06625321e7052c866b53579dd45030f83ca59fa4
DhiaGh <96071963+DhiaGh@users.noreply.github.com>
2023-01-23 18:19:00 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -7,7 +7,7 @@ sudo kubectl create -f config-map.yaml
 sudo kubectl create  -f prometheus-deployment.yaml 
 sudo kubectl get deployments --namespace=monitoring
 sudo kubectl create -f prometheus-service.yaml --namespace=monitoring
-## pour acceder dashboard promeutheus http://@ip publique master:30000/
+echo "pour acceder dashboard promeutheus http://@ip publique master:30000/"
 cd ../
 git clone https://github.com/bibinwilson/kubernetes-grafana.git
 cd kubernetes-grafana/
@@ -16,7 +16,7 @@ sudo kubectl create -f deployment.yaml
 sudo kubectl create -f service.yaml
 sudo kubectl get pods --all-namespaces
 
-## pour acceder dashboard grafana  http://@ip publique master:32000/
+echo "pour acceder dashboard grafana  http://@ip publique master:32000/"
 
 #id template dashboard 8588
 
